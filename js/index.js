bf0ac887366056a586fd43a4df05a2b820c6926b
zesterisk <zesterisk@gmail.com>
2018-01-20 11:55:34 +0800

    forEVEN

---
+++ js/index.js
@@ -0,0 +1,43 @@
+for(var i=11; i<13; i++){
+  $("#board").append("<p>"+i+" - "+i+":50 "+"<input v-model='whatYouRead"+i+"'/>"+"</p>")
+  $("#board").append("<div class='right"+i+"'>" +i+":50時 讀完了：{{whatYouRead"+i+"}}"+"</div>")
+  $(".right"+i).css("color", "rgb("+i*15+","+i+","+i*15+")")
+  $(".right"+i).css("position", "fixed")  
+  $(".right"+i).css("right", "10%")
+  $(".right"+i).css("transform", "translate(0,-40px)")
+}
+
+for(var i=1; i<9; i++){
+  $("#board2").append("<p>"+i+"pm - "+i+":50 "+"<input v-model='whatYouRead"+i+"'/>"+"</p>")
+  $("#board2").append("<div class='right"+i+"'>" +i+":50時 讀完了：{{whatYouRead"+i+"}}"+"</div>")
+  $(".right"+i).css("color", "rgb("+i+","+i*10+","+i*20+")")
+  $(".right"+i).css("position", "fixed")  
+  $(".right"+i).css("right", "10%")
+  $(".right"+i).css("transform", "translate(0,-40px)")
+  // $("#board").append("<p>"+i+"pm - "+i+":50 "+"<textarea rows='3' placeholder='下午讀了哪些呢'></textarea>"+"</p>")
+}
+
+var mydata={
+  whatYouRead11: "認",
+  whatYouRead12: "真"
+}
+var vm=new Vue({
+  el: "#board",
+  data: mydata
+})
+
+var mydata2={
+  whatYouRead1: "看",
+  whatYouRead2: "書",
+  whatYouRead3: "的",
+  whatYouRead4: "男",
+  whatYouRead5: "孩",
+  whatYouRead6: "最",
+  whatYouRead7: "帥",
+  whatYouRead8: "了",
+  whatYouRead9: "哦",
+}
+var vm=new Vue({
+  el: "#board2",
+  data: mydata2
+})
