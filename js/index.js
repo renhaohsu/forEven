8cb977cba15b43ad25cec6b1e8c397796edc2269
zesterisk <zesterisk@gmail.com>
2018-01-22 15:22:10 +0800

    新增reset鍵
    裡面一堆我都覺得是爛code的東西

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 for(var i=11; i<13; i++){
-  $("#board").append("<p>"+i+" - "+i+":50 "+"<input v-model='whatYouRead"+i+"'/>"+"</p>")
+  $("#board").append("<p>"+i+" - "+i+":50 "+"<input v-model='whatYouRead"+i+"'/>"+"<input id='" +i+"' type='reset'>"+"</p>")
   $("#board").append("<div class='right"+i+"'>" +i+":50時 讀完了：{{whatYouRead"+i+"}}"+"</div>")
   $(".right"+i).css("color", "rgb("+i*15+","+i+","+i*15+")")
   $(".right"+i).css("position", "fixed")  
@@ -8,7 +8,7 @@ for(var i=11; i<13; i++){
 }
 
 for(var i=1; i<9; i++){
-  $("#board2").append("<p>"+i+"pm - "+i+":50 "+"<input v-model='whatYouRead"+i+"'/>"+"</p>")
+  $("#board2").append("<p>"+i+"pm - "+i+":50 "+"<input v-model='whatYouRead"+i+"'/>"+"<input id='" +i+"' type='reset'>"+"</p>")
   $("#board2").append("<div class='right"+i+"'>" +i+":50時 讀完了：{{whatYouRead"+i+"}}"+"</div>")
   $(".right"+i).css("color", "rgb("+i+","+i*10+","+i*20+")")
   $(".right"+i).css("position", "fixed")  
@@ -40,4 +40,36 @@ var mydata2={
 var vm=new Vue({
   el: "#board2",
   data: mydata2
+})
+
+
+$("#"+1).click(function(){
+  mydata2.whatYouRead1 = ""
+})
+$("#"+2).click(function(){
+  mydata2.whatYouRead2 = ""
+})
+$("#"+3).click(function(){
+  mydata2.whatYouRead3 = ""
+})
+$("#"+4).click(function(){
+  mydata2.whatYouRead4 = ""
+})
+$("#"+5).click(function(){
+  mydata2.whatYouRead5 = ""
+})
+$("#"+6).click(function(){
+  mydata2.whatYouRead6 = ""
+})
+$("#"+7).click(function(){
+  mydata2.whatYouRead7 = ""
+})
+$("#"+8).click(function(){
+  mydata2.whatYouRead8 = ""
+})
+$("#"+11).click(function(){
+  mydata.whatYouRead11 = ""
+})
+$("#"+12).click(function(){
+  mydata.whatYouRead12 = ""
 })
